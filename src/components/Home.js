327dd7eacb6e5cb17d2c3d1b5ff4b4b0c30e6e3e
xav168 <xav.ong.jh@gmail.com>
2022-10-06 15:31:00 +0800

    Update transition

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 
 function Home() {
   return (
-    <div className="min-h-screen w-screen">
+    <div className="min-h-screen w-screen transition ease-linear duration-300">
       <Navbar />
       <div className="box-border mx-auto p-8 flex justify-items-center justify-center content-center h-[70vh] sm:h-[80vh]">
         <div className="text-5xl sm:text-9xl font-header bg-radius space-x-4 p-2 place-self-center">
@@ -22,7 +22,7 @@ function Home() {
         </div>
       </div>
       <div className="px-8 sm:mb-10 flex flex-col sm:flex-row items-end justify-end text-xl sm:text-2xl font-header mr-5">
-        <div className=" sm:px-10">
+        <div className=" sm:px-10 pb-5">
           <a
             href="https://github.com/xav168"
             className="transition ease-in duration-300 hover:border-b-4 hover:border-black dark:hover:border-white"
@@ -30,7 +30,7 @@ function Home() {
             github.
           </a>
         </div>
-        <div className=" sm:px-10 ">
+        <div className=" sm:px-10 pb-5">
           <a
             href="https://www.linkedin.com/in/xav168/"
             className="transition ease-in duration-300 hover:border-b-4 hover:border-black dark:hover:border-white"
@@ -38,7 +38,7 @@ function Home() {
             linkedin.
           </a>
         </div>
-        <div className=" sm:px-10">
+        <div className=" sm:px-10 pb-5">
           <a
             href={require("../Items/Xavier_Resume.pdf")}
             download={"Xavier_Resume.pdf"}
